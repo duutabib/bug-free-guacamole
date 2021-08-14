d42855f573de648dac89c47ca2eaa2f4340643a9
duutabib <k.aduut@gmail.com>
2021-08-14 15:31:57 +0000

    Replace var newDish, Dishcreate

--- mongoose/index.js
+++ mongoose/index.js
@@ -11,25 +11,23 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 	console.log("Connected to server");
 	
-	var newDish = Dishes({
+	Dishes.create({
 		name: "samolin",
 		description: "salmon with lin",
+	})
+	.then((dish) => {
+		console.log(dish);
+		return Dishes.find({}).exec();
+	})
+	.then((dishes) => {
+		console.log(dishes);
+		return Dishes.deleteOne({});
+	})
+	.then(() => { 	
+		return mongoose.connection.close();
+
+	})
+	.catch((err) => {
+		console.log(err);
 	});
-
-	newDish.save()
-		.then((dish) => {
-			console.log(dish);
-			return Dishes.find({}).exec();
-		})
-		.then((dishes) => {
-			console.log(dishes);
-			return Dishes.deleteOne({});
-		})
-		.then(() => { 	
-			return mongoose.connection.close();
-
-		})
-		.catch((err) => {
-			console.log(err);
-		});
 });
