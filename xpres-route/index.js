50cea341b5d27d445924395f11522c6ce52b691a
duutabib <k.aduut@gmail.com>
2021-08-16 16:11:50 +0000

    Add handlers for promo and leader

--- xpres-route/index.js
+++ xpres-route/index.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 
 
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leader');
+const promoRouter = require('./routes/promo');
 const hostname = 'localhost' ;
 const port = 3000;
 
@@ -24,7 +26,27 @@ app.use((req, res, next) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	
-	res.end('<html><body><h1>Express makes node blazingly fast!</h1></body></html>'); 	
+	res.end('<html><body><h1>dish here: Express makes node blazingly fast!</h1></body></html>'); 	
+});
+
+app.use('/leader', leaderRouter);
+
+app.use((req, res, next) => {
+	console.log(req.headers);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	
+	res.end('<html><body><h1>leader here: Express makes node blazingly fast!</h1></body></html>'); 	
+});
+
+app.use('/promo', promoRouter);
+
+app.use((req, res, next) => {
+	console.log(req.headers);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	
+	res.end('<html><body><h1>promo here: Express makes node blazingly fast!</h1></body></html>'); 	
 });
 
 
