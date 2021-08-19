b72af1e7158d555c1d7e68dc7957863a4dfc6562
duutabib <k.aduut@gmail.com>
2021-08-19 08:53:06 +0000

    Add route for dishId endpoint

--- xpres-route/routes/dishRouter.js
+++ xpres-route/routes/dishRouter.js
@@ -28,4 +28,27 @@ dishRouter.route('/')
 	res.end('deleting all dishes');
 });
 
+// create route for dishId endpoint
+dishRouter.route('/:dishId')
+.all((req, res, next) =>  {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	next();
+})
+.get((req, res, next) => {
+	res.end('sending dishes data through +++========');
+})
+.post((req, res, next) => {
+	res.statusCode = 403;
+	res.end("POST not supported Id endpoint: " + req.params+dishId);
+})
+.put((req, res, next) => {
+	res.write('Update the dish: ' + req.params.dishId + '\n');
+	res.end('Updating dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+	res.end('deleting dish: ' + req.params.dishId);
+});
+
+
 module.exports = dishRouter;
