6990e77311831f05d009793e14f91bbda9e7dfd3
duutabib <k.aduut@gmail.com>
2021-08-23 07:55:19 +0000

    Add model for user

---
+++ conFusionServer/models/user.js
@@ -0,0 +1,25 @@
+// create model for user types
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var User = new Schema({
+	username: {
+			type: String,
+			required: true,
+			unique: true
+		},
+	password: {
+			type: String,
+			required: true
+		},
+	admin:	{
+			type: Boolean,
+			default: false
+		}
+
+});
+
+module.exports = mongoose.model('User', User);
+
