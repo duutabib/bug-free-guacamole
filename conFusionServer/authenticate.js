f36e3d99b535973bc62956063b6e936a7fc6ad70
duutabib <k.aduut@gmail.com>
2021-08-28 14:13:09 +0000

    Add authenticate strategies

---
+++ conFusionServer/authenticate.js
@@ -0,0 +1,10 @@
+// create authenticate strategies file
+
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
