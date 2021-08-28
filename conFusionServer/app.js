264aa40ac6b3ee128f4f4433b81b8cb32847b04b
duutabib <k.aduut@gmail.com>
2021-08-28 14:10:48 +0000

    Modify: passport authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -45,29 +47,22 @@ app.use(session({
 		
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
 //Add authentication --- where does this typical go?
 function auth (req, res, next) {
-	console.log(req.session);
-	if (!req.session.user){
+	if (!req.user){
 		var err = new Error('User not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
+		err.status = 403;
 		return	next(err);
 	} else { 
-		if (req.session.user == 'authenticated') {
 			next();
-		}else {
-			var err=  new Error('User not authenticated');
-			
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 403;
-			return next(err);
-		}
-	}
+	}	
 }
 
 app.use(auth);
